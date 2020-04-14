0ef37467e060c5da8bf433b81d4fcb55c47469a4
Pedro Neves <pefdneves@gmail.com>
2020-04-14 16:31:26 +0200

    Add base app structure
    Signed-off-by: Pedro Neves <pefdneves@gmail.com>

---
+++ app/src/main/java/com/pefdneves/mydots/OverviewActivity.kt
@@ -0,0 +1,12 @@
+package com.pefdneves.mydots
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OverviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_overview)
+    }
+}
